cf32b8cc0a94f18ddad9bf20fdc510e65a317a4c
kirillov <Mikhail.Kirillov@mvideo.ru>
2022-05-31 17:14:38 +0400

    add slow processing

--- src/main/kotlin/com/example/SlowProcessor.kt
+++ src/main/kotlin/com/example/SlowProcessor.kt
@@ -1,4 +1,15 @@
 package com.example
 
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.collectIndexed
+import java.nio.ByteBuffer
+import java.time.Duration
+
 object SlowProcessor {
+    suspend fun process(flow: Flow<ByteBuffer>) {
+        flow.collectIndexed { index, value ->
+            kotlinx.coroutines.time.delay(Duration.ofMinutes(1))
+            println("collected $index buffer ${value.asCharBuffer()}")
+        }
+    }
 }
--- src/main/kotlin/com/example/plugins/Routing.kt
+++ src/main/kotlin/com/example/plugins/Routing.kt
@@ -1,10 +1,14 @@
 package com.example.plugins
 
+import com.example.SlowProcessor
 import io.ktor.server.routing.*
 import io.ktor.http.*
 import io.ktor.server.application.*
 import io.ktor.server.response.*
 import io.ktor.server.request.*
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import java.nio.ByteBuffer
 
 fun Application.configureRouting() {
 
@@ -13,7 +17,30 @@ fun Application.configureRouting() {
         get("/") {
             call.respondText("Hello World!")
         }
+        post("/upload") {
+            val flow = call.receiveFlow()
+            SlowProcessor.process(flow)
+            call.respondText("success process")
+        }
     }
     routing {
+    }
+}
+
+
+fun ApplicationCall.receiveFlow(): Flow<ByteBuffer> = flow<ByteBuffer> {
+
+    val channel = receiveChannel()
+    try {
+        while (true) {
+            val buffer = ByteBuffer.allocate(4096)
+            if (channel.readAvailable(buffer) == -1) {
+                break
+            }
+            buffer.flip()
+            emit(buffer)
+        }
+    } catch (e: Exception) {
+        throw e
     }
 }
